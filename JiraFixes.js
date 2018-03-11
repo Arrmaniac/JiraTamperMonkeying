5a8fb1c24a91d4d605045980902c560c50cba00b
Arrmaniac <arr@gmx.eu>
2018-03-11 15:34:59 +0100

    Create JiraFixes.js with MonkeyScript basics

---
+++ JiraFixes.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         JIRA Fixes
+// @namespace    http://arrmaniac.de
+// @version      0.5
+// @downloadURL    none yet
+// @description  try to take over the world!
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @match        name your JIRA domain
+// @run-at       document-idle
+// ==/UserScript==
+
+(function(){
+  'use strict';
+})();
